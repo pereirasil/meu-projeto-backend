3633b33f4e7e09aa06ae13632886e3ba01f98a81
pereirasil <anderson.informata@gmail.com>
2025-03-30 13:27:02 -0300

    agora estou usando a prorta definida pelo Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3001); // Porta padrão 3000
+// }
+
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001); // Porta padrão 3000
+
+  const PORT = process.env.PORT || 3000; // Usar a porta definida pelo Vercel ou 3000 como fallback
+  await app.listen(PORT); // Porta padrão do Vercel (3000 ou definida pelo Vercel)
+  console.log(`Servidor rodando na porta ${PORT}`);
 }
 
 bootstrap();
